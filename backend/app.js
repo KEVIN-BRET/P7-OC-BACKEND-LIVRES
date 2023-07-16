1a79c12295a0a5b5a3b0c0dfbe94fe9c6e049316
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-07-16 21:26:19 +0200

    Création de la BDD mongoDB et install de mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://kevinbret:tZ1cBvehpjPbLMsn@cluster0.drhwbhs.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 
