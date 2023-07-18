d0ed368b147372208060b8d6aebad5dd906e2993
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-07-18 15:45:22 +0200

    Fichiers restructurés (routes & controllers)

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const Book = require("./models/book");
+const bookRoutes = require("./routes/book-route");
 
 mongoose
   .connect(
@@ -13,8 +13,6 @@ mongoose
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
-
-app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -29,42 +27,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/books", (req, res, next) => {
-  delete req.body.id;
-  const book = new Book({
-    ...req.body,
-  });
-  book
-    .save()
-    .then(() => {
-      res.status(201).json({ message: "Livres enregistré !" });
-    })
-    .catch((error) => res.status(400).json({ error: error }));
-});
+app.use(bodyParser.json());
 
-app.put("/api/books/:id"),
-  (req, res, next) => {
-    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: "Livres modifié !" }))
-      .catch((error) => res.status(400).json({ error }));
-  };
-
-app.delete("/api/books/:id", (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Livre supprimé !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.get("api/books/:id", (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then((book) => res.status(200).json(book))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.get("/api/books", (req, res, next) => {
-  Book.find()
-    .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(404).json({ error }));
-});
+app.use("/api/books", bookRoutes);
 
 module.exports = app;
